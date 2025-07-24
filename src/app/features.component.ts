71d349e5a4c9ac7740e01513bb2f6dacccab50e4
vhifadit <daagbohounondekavhifadit@gmail.com>
2025-07-24 21:57:30 +0100

    Ajout du composant FeaturesComponent (section des fonctionnalités principales)

---
+++ src/app/features.component.ts
@@ -0,0 +1,43 @@
+import { Component } from '@angular/core';
+
+interface Feature {
+  title: string;
+  description: string;
+  icon: string;
+}
+
+@Component({
+  selector: 'app-features',
+  template: `
+    <section class="features-section">
+      <div class="container">
+        <div class="features-grid">
+          <app-feature-card 
+            *ngFor="let feature of features" 
+            [feature]="feature">
+          </app-feature-card>
+        </div>
+      </div>
+    </section>
+  `,
+  styleUrls: ['./features.component.css']
+})
+export class FeaturesComponent {
+  features: Feature[] = [
+    {
+      title: 'Actionable Training',
+      description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+      icon: 'training-icon'
+    },
+    {
+      title: 'Interesting Quizzes',
+      description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+      icon: 'quiz-icon'
+    },
+    {
+      title: 'Premium Material',
+      description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+      icon: 'material-icon'
+    }
+  ];
+} 
